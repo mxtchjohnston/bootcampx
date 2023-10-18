63b8c454375650dfc3495e60a2c7bbbdcda303f2
mxtchjohnston <mitchjohnston99@gmail.com>
2023-10-18 22:59:36 +0000

    queries for teachers and student

--- bootcamp_app/teacher.js
+++ bootcamp_app/teacher.js
@@ -8,11 +8,13 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE $1
-LIMIT $2;
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
 `, process.argv.slice(2))
   .then(res => {
     console.log(res.rows);
